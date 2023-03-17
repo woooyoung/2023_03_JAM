7e2e406abeae8de953e8e5c985fefea49de3e1cb
woooyoung <axdsw121@gmail.com>
2023-03-17 17:55:32 +0900

    JDBC Insert 테스트

--- DB.sql
+++ DB.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS `JAM`;
 CREATE DATABASE `JAM`;
 USE `JAM`;
+
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+SHOW TABLES;
+DESC article;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목 ',RAND()),
+`body` = CONCAT('내용 ',RAND());
+
+SELECT * 
+FROM article;
