97ea3489b60961589569a6c736e40bbb45d06f96
woooyoung <axdsw121@gmail.com>
2023-03-21 15:38:08 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -82,6 +82,60 @@ public class Main {
 				}
 
 				lastArticleId++;
+			} else if (cmd.startsWith("article modify ")) {
+				int id = Integer.parseInt(cmd.split(" ")[2]);
+
+				System.out.println("==게시물 수정==");
+				System.out.printf("새 제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("새 내용 : ");
+				String body = sc.nextLine();
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "UPDATE article";
+					sql += " SET updateDate = NOW(),";
+					sql += " title = '" + title + "',";
+					sql += " `body` = '" + body + "'";
+					sql += " WHERE id = " + id + ";";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+					
+					pstmt.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+				System.out.println(id + "번 글이 수정되었습니다");
+
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
 
