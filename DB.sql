baad2679f0be2a427ba1aeaa0f56f65b2b74b3c1
woooyoung <axdsw121@gmail.com>
2023-03-22 15:08:34 +0900

    DBUtil, SecSql, SqlErrorException 추가

--- DB.sql
+++ DB.sql
@@ -19,5 +19,16 @@ updateDate = NOW(),
 title = CONCAT('제목 ',RAND()),
 `body` = CONCAT('내용 ',RAND());
 
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 112',
+`body` = '내용 12';
+
 SELECT * 
 FROM article;
+
+UPDATE article
+SET updateDate = NOW(),
+title = '제목2'
+WHERE id = 2;
--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
 	public void start() {
 		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
