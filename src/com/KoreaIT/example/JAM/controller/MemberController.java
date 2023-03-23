0b0e79308b6d77089d35e70916867cc6c90b52da
woooyoung <axdsw121@gmail.com>
2023-03-23 15:52:45 +0900

    MVC 구조 도입

--- src/com/KoreaIT/example/JAM/controller/MemberController.java
+++ src/com/KoreaIT/example/JAM/controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.KoreaIT.example.JAM.controller;
 
-import com.KoreaIT.example.JAM.util.DBUtil;
-import com.KoreaIT.example.JAM.util.SecSql;
+import java.sql.Connection;
+import java.util.Scanner;
+
+import com.KoreaIT.example.JAM.service.MemberService;
 
 public class MemberController extends Controller {
+	private MemberService memberService;
+
+	public MemberController(Connection conn, Scanner sc) {
+		super(sc);
+		memberService = new MemberService(conn);
+	}
 
 	public void doJoin(String cmd) {
 		String loginId = null;
@@ -22,13 +30,7 @@ public class MemberController extends Controller {
 				continue;
 			}
 
-			SecSql sql = new SecSql();
-
-			sql.append("SELECT COUNT(*) > 0");
-			sql.append("FROM `member`");
-			sql.append("WHERE loginId = ?", loginId);
-
-			boolean isLoginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+			boolean isLoginIdDup = memberService.isLoginIdDup(loginId);
 
 			if (isLoginIdDup) {
 				System.out.println(loginId + "는(은) 이미 사용중인 아이디입니다");
@@ -81,18 +83,9 @@ public class MemberController extends Controller {
 			break;
 		}
 
-		SecSql sql = new SecSql();
+		int id = memberService.doJoin(loginId, loginPw, name);
 
-		sql.append("INSERT INTO `member`");
-		sql.append("SET regDate = NOW()");
-		sql.append(", updateDate = NOW()");
-		sql.append(", loginId = ?", loginId);
-		sql.append(", loginPw = ?", loginPw);
-		sql.append(", `name` = ?", name);
-
-		int id = DBUtil.insert(conn, sql);
-
-		System.out.println(id + "번 회원님, 가입 되었습니다");
+		System.out.println(name + " 회원님, 가입 되었습니다");
 
 	}
 
