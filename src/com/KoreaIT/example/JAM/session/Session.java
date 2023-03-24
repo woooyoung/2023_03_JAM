140dfa714cdcff7cf9f3ce6dcfc141ccbc8b4e54
woooyoung <axdsw121@gmail.com>
2023-03-24 17:17:47 +0900

    로그인, 로그아웃 체크

--- src/com/KoreaIT/example/JAM/session/Session.java
+++ src/com/KoreaIT/example/JAM/session/Session.java
@@ -11,4 +11,19 @@ public class Session {
 		loginedMemberId = -1;
 	}
 
+	public void logout() {
+		loginedMember = null;
+		loginedMemberId = -1;
+		System.out.println("로그아웃 되었습니다");
+	}
+
+	public void login(Member member) {
+		loginedMember = member;
+		loginedMemberId = member.id;
+	}
+
+	public boolean isLogined() {
+		return loginedMemberId != -1;
+	}
+
 }
